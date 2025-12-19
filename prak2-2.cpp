fd3612738ce02e1ad8af3e924726c24263eae0c6
kim <khisaniputri@gmail.com>
2025-12-19 08:27:01 +0700

    Add files via upload

---
+++ prak2-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string boygrup = "Bangtan Boys";
+    string fandom = "Army";
+    string bias = "Suga";
+    
+    cout << "aku penggemar nomor satu " << boygrup << endl;
+    cout << "dan aku juga " << fandom << "sejati" << endl;
+    cout << "yang paling kusuksai adalah " << bias << endl;
+    
+    cout << "I PURPLE U" << endl;
+    
+    cout << "rekomendasi lagu kesukaan :";
+    cout << "Fire, Drop, Mic drop, Spring day, Gogo, Like, Mikrokosmos, Save me, Light, Life goes on" << endl;
+    
+    string member = "7";
+    
+    cout << "3 + 4 = " << member << "forever" << endl;
+    
+    return 0;
+}
